117a5e40b5172e4429d1312088230f6f2c0ee2c7
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2024-03-31 00:44:13 -0300

    edições finais

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 export const axiosInstance = axios.create({
-    baseURL: 'http://192.168.1.2:8080', 
+    baseURL: process.env.REACT_APP_HOST, 
     timeout: 10000, 
     headers: {
         'Content-Type': 'application/json',
