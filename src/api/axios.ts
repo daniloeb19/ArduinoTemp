f459690a80f570d78c1d138bc295ff7c6fc98c43
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2024-04-05 02:54:04 -0300

    versão estável

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,8 +3,5 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
     baseURL: 'https://back-t6au.onrender.com', 
     timeout: 10000, 
-    headers: {
-        'Content-Type': 'application/json',
-    },
 });
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import { axiosInstance } from '../api/axios';
 import Loading from '../components/Loading';
 import { Picker } from '@react-native-picker/picker';
 import { formatDateToDDMMYYYY } from '../util/Dates';
 import { Data, HomeData } from '../interfaces/Temp';
 import { Button } from 'react-native-elements';
+import { Dimensions } from 'react-native';
+import {
+    TouchableWithoutFeedback
+} from 'react-native';
+import SyncStorage from 'sync-storage';
 
 const Home: React.FC = () => {
     const [loadingState, setLoadingState] = useState<"error" | "ok" | "loading">("loading");
@@ -15,6 +20,40 @@ const Home: React.FC = () => {
     const [selectedNumber, setSelectedNumber] = useState<number>(10);
     const [error, setError] = useState<string>("");
     const [reload, setReload] = useState<number>(0);
+    const colors = [
+        { background: '#6874e7', text: '#FFFFFF' },
+        { background: '#b8304f', text: '#FFFFFF' },
+        { background: '#758E4F', text: '#000000' },
+        { background: '#fa3741', text: '#FFFFFF' },
+        { background: '#F26419', text: '#FFFFFF' },
+        { background: '#F6AE2D', text: '#000000' },
+        { background: '#DFAEB4', text: '#000000' },
+        { background: '#7A93AC', text: '#000000' },
+        { background: '#33658A', text: '#FFFFFF' },
+        { background: '#3d2b56', text: '#FFFFFF' },
+        { background: '#42273B', text: '#FFFFFF' },
+        { background: '#171A21', text: '#FFFFFF' },
+        { background: '#E52B50', text: '#FFFFFF' },
+        { background: '#FFBF00', text: '#000000' },
+        { background: '#9966CC', text: '#000000' },
+        { background: '#FBCEB1', text: '#000000' },
+        { background: '#7FFFD4', text: '#000000' },
+        { background: '#50C878', text: '#000000' },
+        { background: '#6B8E23', text: '#000000' },
+        { background: '#4682B4', text: '#000000' },
+        { background: '#B0C4DE', text: '#000000' }
+    ]
+        ;
+
+
+    const CIRCLE_SIZE = 40;
+    const CIRCLE_RING_SIZE = 2;
+    const [value, setValue] = React.useState(0);
+
+    useEffect(() => {
+        setValue(SyncStorage.get('color-temp') || 0);
+    }, [SyncStorage]);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -67,16 +106,31 @@ const Home: React.FC = () => {
 
     const styles = StyleSheet.create({
         container: {
+            width: "auto",
+            height: "100%",
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
+        },
+        containerEnd: {
+            width: "auto",
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
+            marginTop: "auto",
+        },
+        loading: {
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
         },
         buttonReloadContainer: {
             width: '100%',
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
         },
         buttonReload: {
+            width: "100%",
             color: "white",
-            backgroundColor: "black",
+            backgroundColor: "#000000",
             height: 50,
             borderTopLeftRadius: 0,
             borderTopRightRadius: 0,
@@ -86,25 +140,102 @@ const Home: React.FC = () => {
         dropdown: {
             height: 50,
             width: 400,
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
         },
         center: {
             justifyContent: 'center',
             alignItems: 'center',
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
         },
         leftContainer: {
-            alignItems: "flex-start"
+            alignItems: "flex-start",
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
         },
         data: {
             fontSize: 24,
             fontWeight: 'bold',
-            margin:8
+            margin: 8,
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
         },
-        flex:{
+        flex: {
             justifyContent: "space-evenly",
             display: "flex",
-            flexDirection: "row"
-        }
+            flexDirection: "row",
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
+        },
+        group: {
+            flexDirection: 'row',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            marginBottom: 12,
+            backgroundColor: colors[value].background,
+            color: colors[value].text,
+        },
+
+        placeholder: {
+            flexGrow: 1,
+            flexShrink: 1,
+            flexBasis: 0,
+            height: 400,
+            marginTop: 0,
+            padding: 24,
+            backgroundColor: 'transparent',
+        },
+        circle: {
+            width: CIRCLE_SIZE + CIRCLE_RING_SIZE * 4,
+            height: CIRCLE_SIZE + CIRCLE_RING_SIZE * 4,
+            borderRadius: 9999,
+            backgroundColor: 'white',
+            borderWidth: CIRCLE_RING_SIZE,
+            borderColor: 'transparent',
+            marginRight: 8,
+            marginBottom: 12,
+        },
+        circleInside: {
+            width: CIRCLE_SIZE,
+            height: CIRCLE_SIZE,
+            borderRadius: 9999,
+            position: 'absolute',
+            top: CIRCLE_RING_SIZE,
+            left: CIRCLE_RING_SIZE,
+        },
+        btn: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 6,
+            padding: 14,
+            borderWidth: 1,
+            borderColor: '#000',
+            backgroundColor: '#000',
+            marginBottom: 12,
+        },
+        btnText: {
+            fontSize: 16,
+            fontWeight: '600',
+            color: '#fff',
+        },
     });
+
+
+    const showError = () => {
+        Alert.alert(
+            'Error',
+            error,
+            [{
+                onPress() {
+                    setError("");
+                    setReload((prev) => prev + 1);
+                    setLoadingState("loading");
+                },
+                text: 'Recarregar',
+                style: 'destructive',
+            },])
+    };
 
     return (
         <View style={styles.container} >
@@ -115,7 +246,8 @@ const Home: React.FC = () => {
                     onPress={() => setReload((prev) => prev + 1)}
                 />
             </View>
-            <View style={styles.center}>
+            <View style={styles.leftContainer}>
+
                 {dateData && responseData && (
                     <Picker
                         selectedValue={selectedDate}
@@ -172,7 +304,7 @@ const Home: React.FC = () => {
 
             {
                 loadingState === "loading" && (
-                    <View style={styles.container}>
+                    <View style={styles.loading}>
                         <View style={styles.center}>
                             <Loading />
                         </View>
@@ -180,13 +312,40 @@ const Home: React.FC = () => {
                 )
             }
 
-            <View style={styles.container}>
-                <Text>{error.toString()}</Text>
+            {error !== "" && (<View>
+                <>{showError()}</>
+            </View>)}
+
+            <View style={styles.containerEnd}>
+                <View style={styles.group}>
+                    {colors.map((item, index) => {
+                        const isActive = value === index;
+                        return (
+                            <View key={item.background}>
+                                <TouchableWithoutFeedback
+                                    onPress={() => {
+                                        setValue(index);
+                                        SyncStorage.set('color-temp', index);
+
+                                    }}>
+                                    <View
+                                        style={[
+                                            styles.circle,
+                                            isActive && { borderColor: item.background },
+                                        ]}>
+                                        <View
+                                            style={[styles.circleInside, { backgroundColor: item.background }]}
+                                        />
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </View>
+                        );
+                    })}
+                </View>
             </View>
         </View >
     );
 };
-
 
 
 export default Home;
